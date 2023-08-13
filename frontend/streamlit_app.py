cc1ef78b5ec87ba6d61db9b4ebfd89f0cbff9fa5
prashant-devops <71997822+prashant-devops@users.noreply.github.com>
2023-08-12 20:32:19 -0400

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,43 +1,18 @@
 import openai
-
 import streamlit as st
 
- 
-
 openai.api_key = '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'
-
- 
-
 st.title('Automated Code Generator')
-
- 
-
 user_input = st.text_input('Describe what you want the code to do:')
 
- 
-
 if user_input:
-
     response = openai.Completion.create(
-
         engine='text-davinci-003',
-
         prompt=f'Generate Python code for: {user_input}',
-
         max_tokens=500,
-
         temperature=0.5
-
     )
-
-    
-
     generated_code = response['choices'][0]['text'].strip()
-
-    
-
     st.code(generated_code, language='python')
 
-    
-
-st.write('Created by Dheeraj Pagare and Prashant Kumar Jha')
+st.write('Created by Group 7: Dheeraj Pagare and Prashant Kumar Jha')
